2b6077144d5b266ec48faa5392b260d7fdbe9f0f
winshaS8 <72268258+winshaS8@users.noreply.github.com>
2022-08-24 19:43:58 +0530

    Add files via upload

---
+++ pro102.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+from_dir = "C:\Users\WELCOME PC"
+to_dir = "C:\Users\WELCOME PC\Desktop\class projects"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+
+    name, extension = os.splitext(file_name)
+    print(name)
+    print(extension)
+
+    if extension == "":
+       continue
+    if extension in ['.gif','.png','.jpg','.jpeg','.jfif']:
+       path1 = from_dir + '/' + file_name
+       path2 = to_dir + '/' + "Image_Files"
+       path3 = to_dir + '/' +"Image_Files" +  file_name
+       print("path1" , path1)
+       print("path3", path3)
+
+    if os.path.exists(path2):
+      print("Moving" + file_name + "......")  
+      shutil.move(path1, path3)
+    else:
+      os.mkdirs(path2)
+      print("Moving" + file_name + ".....")
+      shutil.move(path1, path3)
